09ca7125f63448931d70ae91e849810ac4a368b6
Aaron Lisker <aelisker@gmail.com>
2020-04-12 12:09:15 -0700

    added ability to find feature repos by preset languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSeachTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formHandlerSubmit = function(event) {
     event.preventDefault();
@@ -16,6 +17,16 @@ var formHandlerSubmit = function(event) {
     else {
         alert("Please enter a GitHub username");
     }
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute('data-language');
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = '';
+    }  
 };
 
 var getUserRepos = function(user) {
@@ -39,6 +50,21 @@ var getUserRepos = function(user) {
             //notice this `.catch()` getting chained onto the end of the `.then()` method
             alert("Unable to connect to Github");
         });
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });        
+        }
+        else {
+            alert('Error: ' + response.statusText);
+        }
+    });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -90,4 +116,5 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formHandlerSubmit);
